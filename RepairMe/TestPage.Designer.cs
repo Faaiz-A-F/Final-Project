6ed0248733608af3581b0fc3536cf2485a60013a
Faaiz-A-F <faaizaf@students.amikom.ac.id>
2024-12-07 22:16:43 +0700

    Koneksi MySQL Test Berhasil

---
+++ RepairMe/TestPage.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace RepairMe
+{
+    partial class TestPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTestConnextion = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTestConnextion
+            // 
+            this.btnTestConnextion.Location = new System.Drawing.Point(283, 164);
+            this.btnTestConnextion.Name = "btnTestConnextion";
+            this.btnTestConnextion.Size = new System.Drawing.Size(216, 104);
+            this.btnTestConnextion.TabIndex = 0;
+            this.btnTestConnextion.Text = "Test";
+            this.btnTestConnextion.UseVisualStyleBackColor = true;
+            this.btnTestConnextion.Click += new System.EventHandler(this.btnTestConnextion_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnTestConnextion);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTestConnextion;
+    }
+}
---
+++ RepairMe/TestPage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using RepairMe.Model.Context; // Namespace where DbContext is defined
+
+namespace RepairMe
+{
+    public partial class TestPage : Form
+    {
+        public TestPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTestConnextion_Click(object sender, EventArgs e)
+        {
+            using (var dbContext = new DbContext())
+            {
+                try
+                {
+                    // Attempt to open the connection
+                    dbContext.OpenConnection();
+                    MessageBox.Show("Connection successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    // Display error message if connection fails
+                    MessageBox.Show($"Failed to connect to the database.\n\nError: {ex.Message}",
+                                    "Connection Error",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
+   
+
