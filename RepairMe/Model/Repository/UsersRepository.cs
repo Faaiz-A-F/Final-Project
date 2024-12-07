e75bcd4c9bc8a61e318237493b69284dfc327ed6
Faaiz-A-F <faaizaf@students.amikom.ac.id>
2024-12-07 21:21:32 +0700

    Database Test

---
+++ RepairMe/Model/Repository/UsersRepository.cs
@@ -0,0 +1,18 @@
+﻿using RepairMe.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RepairMe.Model.Entity;
+using RepairMe.Model.Context;
+
+namespace RepairMe.Model.Repository
+{
+    internal class UsersRepository
+    {
+        
+    }
+}
