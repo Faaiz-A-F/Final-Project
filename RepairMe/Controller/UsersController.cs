e65f5e3a5bda1c35b04353e0ed66efd8ba56f42b
Fatih iqbal <fatihiqbal622@students.amikom.ac.id>
2024-12-13 19:49:30 +0700

    biar bisa ngepull

--- RepairMe/Controller/UsersController.cs
+++ RepairMe/Controller/UsersController.cs
@@ -63,6 +63,7 @@ namespace RepairMe.Controller
                     Username = username,
                     Password = password,
                     Age = age,
+                    Name = name,
                     Email = email,
                     Phone = phone,
                     Address = address,
