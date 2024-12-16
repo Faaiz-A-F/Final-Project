b0f346b5022dcb75ebce82ec709bd344e71ead9a
Faaiz-A-F <faaizaf@students.amikom.ac.id>
2024-12-16 21:25:18 +0700

    update database sql

--- RepairMe/Database/repairme.sql
+++ RepairMe/Database/repairme.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 07, 2024 at 01:55 PM
+-- Generation Time: Dec 16, 2024 at 03:24 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,50 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `jasa_bengkel`
+--
+
+CREATE TABLE `jasa_bengkel` (
+  `jasa_id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `harga` decimal(8,2) NOT NULL,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `motor`
+--
+
+CREATE TABLE `motor` (
+  `motor_id` int(11) NOT NULL,
+  `merk` varchar(255) NOT NULL,
+  `kapasitas` varchar(255) NOT NULL,
+  `jenis` enum('matic','bebek','fairing','naked') NOT NULL,
+  `tahun` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `transaksi_id` int(11) NOT NULL,
+  `tanggal_transaksi` timestamp NOT NULL DEFAULT current_timestamp(),
+  `total` decimal(10,2) NOT NULL,
+  `status` enum('pending','done') NOT NULL,
+  `review` varchar(255) DEFAULT NULL,
+  `rating` int(5) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `motor_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -33,15 +77,43 @@ CREATE TABLE `users` (
   `email` varchar(100) DEFAULT NULL,
   `address` varchar(100) DEFAULT NULL,
   `age` int(3) DEFAULT NULL,
-  `phone` int(50) DEFAULT NULL,
+  `phone` varchar(50) DEFAULT NULL,
   `password` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `role` enum('user','admin') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `name`, `email`, `address`, `age`, `phone`, `password`, `created_at`, `role`) VALUES
+(2, 'Faaiz', 'faaizaf@students.amikom.ac.id', 'Sleman', 20, '085156205044', '1234', '2024-12-16 12:53:04', 'user');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `jasa_bengkel`
+--
+ALTER TABLE `jasa_bengkel`
+  ADD PRIMARY KEY (`jasa_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `motor`
+--
+ALTER TABLE `motor`
+  ADD PRIMARY KEY (`motor_id`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`transaksi_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `motor_id` (`motor_id`);
 
 --
 -- Indexes for table `users`
@@ -54,10 +126,45 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `jasa_bengkel`
+--
+ALTER TABLE `jasa_bengkel`
+  MODIFY `jasa_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `motor`
+--
+ALTER TABLE `motor`
+  MODIFY `motor_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `transaksi_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `jasa_bengkel`
+--
+ALTER TABLE `jasa_bengkel`
+  ADD CONSTRAINT `jasa_bengkel_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`motor_id`) REFERENCES `motor` (`motor_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
