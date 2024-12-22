af11fc88d853a6f9eb541ad1986bc6659e89f629
Faaiz-A-F <faaizaf@students.amikom.ac.id>
2024-12-22 16:17:05 +0700

    update notes & comment

---
+++ RepairMe/Model/Repository/MotorRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepairMe.Model.Repository
+{
+    internal class MotorRepository
+    {
+        // sabar kang iseh kesel, engko bengi tak garap e (22/12/2024)
+    }
+}
